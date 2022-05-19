9a52f2ce9b920a8866ff22b682a4389985957f57
fshangala <fshangala@gmail.com>
2022-05-19 04:55:21 +0200

    fshangala branch initialized basic view

--- App.tsx
+++ App.tsx
@@ -1,18 +1,42 @@
 import * as React from 'react';
 import {
+  Appbar,
   Provider as PaperProvider
 } from 'react-native-paper';
 import {
   NavigationContainer
 } from '@react-navigation/native';
+import { 
+  View,
+  Text
+} from 'react-native';
+import {
+  createNativeStackNavigator
+} from '@react-navigation/native-stack';
+import {
+  Dashboard
+} from './components/dashboard';
+
+const Stack = createNativeStackNavigator();
+
+function CustomHeaderBar({navigation,back}:any) {
+  return (
+    <Appbar.Header>
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title="Accademica" />
+    </Appbar.Header>
+  )
+}
 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <View>
-          <Text>Open up App.tsx to start working on your app!</Text>
-        </View>
+        <Stack.Navigator screenOptions={{
+          header: (props:any)=> <CustomHeaderBar {...props} />,
+        }}>
+          <Stack.Screen name='Dashboard' component={Dashboard} />
+        </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
